9ccf12baf063750351a67871778ad022e1770992
Hakimah28 <mahb2205888@student.ctu.edu.vn>
2025-11-06 10:49:58 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-// Gắn router cho tài nguyên contacts
 app.use("/api/contacts", contactsRouter);
 
-// Middleware xử lý lỗi 404 – không tìm thấy route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to contact book application." });
+});
+
 app.use((req, res, next) => {
   return next(new ApiError(404, "Resource not found"));
 });
-
-// Middleware xử lý lỗi tổng quát
 app.use((err, req, res, next) => {
+
   return res.status(err.statusCode || 500).json({
     message: err.message || "Internal Server Error",
   });
--- app/config/index.js
+++ app/config/index.js
@@ -1,7 +1,10 @@
 const config = {
-    app: {
-        port: process.env.PORT || 3000,
-    }
+  app: {
+    port: process.env.PORT || 3000,
+  },
+  db: {
+    uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/CT449-B2205888'
+  }
 };
 
 module.exports = config;
